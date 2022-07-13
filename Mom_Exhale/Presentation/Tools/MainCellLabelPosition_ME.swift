0b28cc3494cd886b22f9a2d6f19394ed3d3aca15
Max <4440449@gmail.com>
2022-07-14 01:09:59 +0300

    added entity for the MainCellLabel position

---
+++ Mom_Exhale/Presentation/Tools/MainCellLabelPosition_ME.swift
@@ -0,0 +1,15 @@
+//
+//  MainCellLabelPosition_ME.swift
+//  Mom_Exhale
+//
+//  Created by Maxim on 13.07.2022.
+//
+
+import UIKit
+
+
+enum MainCellLabelPosition_ME {
+    case topLeading
+    case bottomTrailing
+    case center
+}
