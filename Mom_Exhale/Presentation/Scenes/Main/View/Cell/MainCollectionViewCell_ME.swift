e08fe2cdac2909f7ad0f304287fcc01df2c108e5
Max <4440449@gmail.com>
2022-07-14 00:54:22 +0300

    fixed UI with Figma design guideline

--- Mom_Exhale/Presentation/Scenes/Main/View/Cell/MainCollectionViewCell_ME.swift
+++ Mom_Exhale/Presentation/Scenes/Main/View/Cell/MainCollectionViewCell_ME.swift
@@ -17,13 +17,9 @@ class MainCollectionViewCell_ME: UICollectionViewCell {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = .white
-        self.layer.cornerRadius = 15
-        self.layer.shadowColor = UIColor.black.cgColor
-        self.layer.shadowRadius = 10
-        self.layer.shadowOpacity = 0.1
-        self.layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+        contentView.addSubview(moduleImage)
         contentView.addSubview(moduleLabel)
+        setupInitialUI()
         setupLayout()
     }
     
@@ -36,19 +32,46 @@ class MainCollectionViewCell_ME: UICollectionViewCell {
 
     private lazy var moduleLabel: UILabel = {
         let label = UILabel()
-        label.text = "Test"
-        label.textColor = .black
-        label.numberOfLines = 2
-        label.textAlignment = .left
+        label.font = UIFont(name: "Montserrat-Regular", size: 13)!
+        label.textColor = .label
+        label.numberOfLines = 1
+        label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    private func setupLayout() {
-        moduleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        moduleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
-        moduleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 10).isActive = true
+    private lazy var moduleImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    
+    // MARK: - Initital UI
+
+    func setupInitialUI() {
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 19
+        //TODO: shadow color
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowRadius = 10
+        self.layer.shadowOpacity = 0.1
+        self.layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+        contentView.layer.cornerRadius = 18
     }
+    
+
+    // MARK: - Layout
+    
+    private func setupLayout() {
+        moduleImage.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        moduleImage.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        moduleImage.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        moduleImage.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+    }
+    
+    
+    // MARK: - Animation
     
     override var isHighlighted: Bool {
       didSet {
@@ -61,8 +84,38 @@ class MainCollectionViewCell_ME: UICollectionViewCell {
     }
     
     
-    func setupTextLabel(text: String) {
+    // MARK: - External handling
+    
+    func setupCellUI(position: MainCellLabelPosition_ME,
+                     text: String,
+                     textColor: UIColor,
+                     image: UIImage) {
         moduleLabel.text = text
+        moduleLabel.textColor = textColor
+        moduleImage.image = image
+        switch position {
+        case .topLeading:
+            moduleLabel.topAnchor.constraint(equalTo: self.topAnchor,
+                                             constant: 15).isActive = true
+            moduleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor,
+                                                 constant: 15).isActive = true
+            moduleLabel.trailingAnchor.constraint(lessThanOrEqualTo: self.trailingAnchor,
+                                                  constant: -5).isActive = true
+        case .bottomTrailing:
+            moduleLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor,
+                                                constant: -15).isActive = true
+            moduleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor,
+                                                  constant: -15).isActive = true
+            moduleLabel.leadingAnchor.constraint(greaterThanOrEqualTo: self.leadingAnchor, constant: 5).isActive = true
+        case .center:
+            moduleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor,
+                                                 constant: 5).isActive = true
+            moduleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor,
+                                                  constant: -5).isActive = true
+            moduleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        }
     }
 
 }
+
+
